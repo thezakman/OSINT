33c8e5a75ea726b2f6664c40bf394a40f5ea9857
Pedro L.C.A <thezakman@icloud.com>
2022-02-07 23:16:43 -0300

    quick fix

--- OSINT.py
+++ OSINT.py
@@ -19,17 +19,18 @@ print("""\
 
 host = input("HOST: ")
 
-titulos = ["[Documentos]", "[Listagem de Diretório]", "[Arquivos de Config]","[Databases]","[Backups]","[Paginas de Login]","[Erros]","[SQLi]","[HAILMARY?]","[REPOSITÓRIOS]"]
+titulos = ["[Documentos]", "[Listagem de Diretório]", "[Arquivos de Config]","[Databases]","[Backups]","[Paginas de Login]","[Erros]","[SQLi]","[REPOSITÓRIOS]","[HAILMARY?]"]
 dorks = ['site:{} ext:doc | ext:docx | ext:odt | ext:rtf | ext:sxw | ext:psw | ext:ppt | ext:pptx | ext:pps | ext:csv | ext:pdf'.format(str(host)),
          'site:{} intitle:index.of'.format(str(host)),
          'site:{} ext:xml | ext:conf | ext:cnf | ext:reg | ext:inf | ext:rdp | ext:cfg | ext:txt | ext:ora | ext:ini | ext:env | ext:log'.format(str(host)),
          'site:{} ext:sql | ext:dbf | ext:mdb'.format(str(host)),
          'site:{} ext:bkf | ext:bkp | ext:bak | ext:old | ext:backup'.format(str(host)),
          'site:{} inurl:login | inurl:signin | intitle:Login | intitle:"sign in" | inurl:auth | intitle:"acessar" | intitle:"entrar"'.format(str(host)),
-         'site:{} "PHP Parse error" | "PHP Warning" | "PHP Error" | intext:"sql syntax near" | intext:"syntax error has occurred" | intext:"incorrect syntax near" | intext:"unexpected end of SQL command" | intext:"Warning: mysql_connect()" | intext:"Warning: mysql_query()" | intext:"Warning: pg_connect()"'.format(str(host)),
+         #'site:{} "PHP Parse error" | "PHP Warning" | "PHP Error" | intext:"sql syntax near" | intext:"syntax error has occurred" | intext:"incorrect syntax near" | intext:"unexpected end of SQL command" | intext:"Warning: mysql_connect()" | intext:"Warning: mysql_query()" | intext:"Warning: pg_connect()"'.format(str(host)),
+         'site:{} intext:error | intext:sql'.format(str(host)),
          'site:{} ext:php | ext:asp | ext:aspx | ext:php5'.format(str(host)),
-         'site:pastebin.com | site:postman.com | site:paste2.org | site:pastehtml.com | site:slexy.org | site:snipplr.com | site:snipt.net | site:textsnip.com | site:bitpaste.app | site:justpaste.it | site:heypasteit.com | site:hastebin.com | site:dpaste.org | site:dpaste.com | site:codepad.org | site:jsitor.com | site:codepen.io | site:jsfiddle.net | site:dotnetfiddle.net | site:phpfiddle.org | site:ide.geeksforgeeks.org | site:repl.it | site:ideone.com | site:paste.debian.net | site:paste.org | site:paste.org.ru | site:codebeautify.org  | site:codeshare.io | site:trello.com "{}"'.format(str(host)),
-         'site:github.com | site:gitlab.com intext:{}'.format(str(host))
+         'site:github.com | site:gitlab.com intext:{}'.format(str(host)),
+         'site:pastebin.com | site:postman.com | site:paste2.org | site:pastehtml.com | site:slexy.org | site:snipplr.com | site:snipt.net | site:textsnip.com | site:bitpaste.app | site:justpaste.it | site:heypasteit.com | site:hastebin.com | site:dpaste.org | site:dpaste.com | site:codepad.org | site:jsitor.com | site:codepen.io | site:jsfiddle.net | site:dotnetfiddle.net | site:phpfiddle.org | site:ide.geeksforgeeks.org | site:repl.it | site:ideone.com | site:paste.debian.net | site:paste.org | site:paste.org.ru | site:codebeautify.org  | site:codeshare.io | site:trello.com {}'.format(str(host))
          ]
          
 
